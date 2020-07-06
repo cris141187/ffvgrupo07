5e6c65451e6830a326a8737fe1d58303e3209d2b
Estudo <Estudo@Estudo-PC>
2020-07-06 11:26:18 -0300

    Atualizado 06/07

---
+++ scel/src/main/java/com/fatec/scel/model/EmprestimoRepository.java
@@ -0,0 +1,7 @@
+package com.fatec.scel.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface EmprestimoRepository extends CrudRepository<Emprestimo, Long> {
+}
